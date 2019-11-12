c34344916dfae3817e62f29a35219c76cbdb71eb
Sebastian Rueda <jrueda275@unab.edu.co>
2019-11-12 00:51:28 -0500

    Assasins mist completed

---
+++ Souls Link/Assets/Scripts/Skills/Assasin/Mist.cs
@@ -0,0 +1,107 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mist : MonoBehaviour
+{
+    public float _coolDown = 0;
+    public float _stealthTime = 0;
+    public float _mistLifeTime = 0;
+    [SerializeField] private KeyCode _inputAttack;
+    public GameObject _mist;
+    private float _coolDownTracker = 0;
+    private float _stealthTimeTracker = 0;
+    [Header("Offset Y of mist spawn")]
+    public float offsetY = 0;
+
+    private bool isStealth = false;
+    private bool insideMist = false;
+    public bool IsStealth { get => isStealth; set => isStealth = value; }
+    public bool InsideMist { get => insideMist; set => insideMist = value; }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _coolDownTracker = _coolDown;
+        _stealthTimeTracker = _stealthTime;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        checkStealth();
+
+        if (_coolDownTracker <= _coolDown && _coolDownTracker > 0)
+        {
+            _coolDownTracker -= Time.deltaTime;
+        }
+
+        if (Input.GetKeyDown(_inputAttack) && _coolDownTracker <= 0)
+        {
+            spawnMist();
+            _coolDownTracker = _coolDown;
+        }
+
+        //Debug.Log("Estoy invisible?: " + isStealth);
+    }
+
+    private void spawnMist()
+    {
+        Vector3 newMistPosition = new Vector3(transform.position.x, transform.position.y + offsetY, transform.position.z);
+        GameObject temp = Instantiate(_mist, newMistPosition, Quaternion.identity);
+        StartCoroutine(destroyMist(temp, _mistLifeTime));
+    }
+
+    IEnumerator destroyMist(GameObject mist, float time)
+    {
+        yield return new WaitForSeconds(time);
+        Destroy(mist);
+    }
+
+    private void checkStealth()
+    {
+        if (!insideMist)
+        {
+            if (isStealth)
+            {
+                Color tmp = GetComponent<SpriteRenderer>().color;
+                tmp.a = 0.5f;
+                GetComponent<SpriteRenderer>().color = tmp;
+
+                if (_stealthTimeTracker <= _stealthTime && _stealthTime > 0)
+                {
+                    _stealthTimeTracker -= Time.deltaTime;
+                }
+
+                if (_stealthTimeTracker <= 0)
+                {
+                    isStealth = false;
+                    _stealthTimeTracker = _stealthTime;
+                }
+            }
+            else
+            {
+                Color tmp = GetComponent<SpriteRenderer>().color;
+                tmp.a = 1f;
+                GetComponent<SpriteRenderer>().color = tmp;
+            }
+        }
+        else
+        {
+            isStealth = true;
+        }
+    }
+
+    public void activateInsideMist()
+    {
+        insideMist = true;
+        _stealthTimeTracker = _stealthTime;
+    }
+
+    public void resetStealth()
+    {
+        insideMist = false;
+        isStealth = false;
+        _stealthTimeTracker = _stealthTime;
+    }
+}
---
+++ Souls Link/Assets/Scripts/Skills/Assasin/MistAnimations.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MistAnimations : MonoBehaviour
+{
+    private bool isColliding = false;
+    private GameObject player;
+
+    public void completeAnimation()
+    {
+        GetComponent<Animator>().SetBool("CreationCompleted", true);
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            player = collision.gameObject;
+            player.GetComponent<Mist>().activateInsideMist();
+        }
+        isColliding = true;
+    }
+
+    public void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            collision.gameObject.GetComponent<Mist>().resetStealth();
+            collision.gameObject.GetComponent<Mist>().IsStealth = true;
+            isColliding = false;
+        }
+    }
+
+    private void OnDisable()
+    {
+        if (isColliding)
+        {
+            player.GetComponent<Mist>().resetStealth();
+            player.GetComponent<Mist>().IsStealth = true;
+        }
+    }
+}
