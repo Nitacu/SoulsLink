85feaeff65d846e060192a740fc2c7ee719a3027
Sebastian Rueda <jrueda275@unab.edu.co>
2019-11-29 18:10:10 -0500

    NewTestSceneUpdate

---
+++ Souls Link/Assets/Scripts/UI/TestDropDownScene/DropDownList.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class DropDownList : MonoBehaviour
+{
+    List<string> options = new List<string>() { "Photon", "Socket" };
+    private TMP_Dropdown _dropdown;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _dropdown = GetComponent<TMP_Dropdown>();
+        fillList();
+    }
+
+    private void fillList()
+    {        
+        _dropdown.AddOptions(options);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
