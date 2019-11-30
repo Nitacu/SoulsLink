a13b4ba61f95e3c34c0d051bc5b40283ca2f7ccb
Kevin Castellanos <kcastellanos278@unab.edu.co>
2019-11-29 21:14:04 -0500

    Spawn de enemigos con photon

--- Souls Link/Assets/Scripts/Control/Arena/ControlSpawnEnemys.cs
+++ Souls Link/Assets/Scripts/Control/Arena/ControlSpawnEnemys.cs
@@ -2,31 +2,74 @@
 using System.Collections.Generic;
 using UnityEngine;
 using SWNetwork;
+using Photon.Pun;
 
 public class ControlSpawnEnemys : MonoBehaviour
 {
-    [SerializeField] private int _numberEnemys = 6;
+    [SerializeField] private int _numberEnemys;
     public SceneSpawner spawner;
+    [Header("Cosas del Photon")]
+    public PhotonView _photonView;
+    [SerializeField] private List<Transform> _points = new List<Transform>();
+    [SerializeField] private List<string> _enemys = new List<string>();
 
+    private void Start()
+    {
+        onSpawnPlayerWithPhoton();
+    }
+
+    //para crear uno nuevo a medida que los van matando
     public void spawnRandomEnemy()
     {
-        if (NetworkClient.Instance.IsHost)
+        if (GameManager.GetInstace()._multiplayerServer == GameManager.MultiplayerServer.SOCKETWEAVER)
         {
-            int random = Random.Range(0, spawner.NumberOfSpawnPoints);
-            int randomEnemy = Random.Range(0, spawner.NumberOfNonPlayerPrefabs);
-            NetworkClient.Instance.FindSpawner(1).SpawnForNonPlayer(randomEnemy, random);
+            if (NetworkClient.Instance.IsHost)
+            {
+                int random = Random.Range(0, spawner.NumberOfSpawnPoints);
+                int randomEnemy = Random.Range(0, spawner.NumberOfNonPlayerPrefabs);
+                NetworkClient.Instance.FindSpawner(1).SpawnForNonPlayer(randomEnemy, random);
+            }
+        }
+        else
+        {
+            if (PhotonNetwork.IsMasterClient)
+            {
+                int random = Random.Range(0, _points.Count);
+                int randomEnemy = Random.Range(0, _enemys.Count);
+                PhotonNetwork.Instantiate(_enemys[randomEnemy], _points[random].position, Quaternion.identity, 0);
+
+            }
+        }
+    }
+
+    //cuando inica el juego
+    public void onSpawnerReady(bool alreadyFinishedSceneSetup)
+    {
+        if (GameManager.GetInstace()._multiplayerServer == GameManager.MultiplayerServer.SOCKETWEAVER)
+        {
+            if (NetworkClient.Instance.IsHost)
+            {
+                for (int i = 0; i < _numberEnemys; i++)
+                {
+                    int random = Random.Range(0, spawner.NumberOfNonPlayerPrefabs);
+                    NetworkClient.Instance.FindSpawner(1).SpawnForNonPlayer(random, i);
+                }
+            }
         }
 
     }
 
-    public void onSpawnerReady(bool alreadyFinishedSceneSetup)
+    public void onSpawnPlayerWithPhoton()
     {
-        if (NetworkClient.Instance.IsHost)
+        if (GameManager.GetInstace()._multiplayerServer == GameManager.MultiplayerServer.PHOTON)
         {
-            for (int i = 0; i < _numberEnemys; i++)
+            if (PhotonNetwork.IsMasterClient)
             {
-                int random = Random.Range(0, spawner.NumberOfNonPlayerPrefabs);
-                NetworkClient.Instance.FindSpawner(1).SpawnForNonPlayer(random, i);
+                for (int i = 0; i < _numberEnemys; i++)
+                {
+                    int random = Random.Range(0, _enemys.Count);
+                    PhotonNetwork.Instantiate(_enemys[random], _points[i].position, Quaternion.identity, 0);
+                }
             }
         }
     }
