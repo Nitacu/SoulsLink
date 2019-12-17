d442bd47f7fa6219a8537e1f1a8c426a720cf8fc
Kevin Castellanos <kcastellanos278@unab.edu.co>
2019-12-17 17:08:58 -0500

    Ping

---
+++ Souls Link/Assets/Scripts/Control/Multiplayer/ShowPing.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using TMPro;
+
+public class ShowPing : MonoBehaviour
+{
+    [SerializeField] private TMP_Text text;
+
+    private void FixedUpdate()
+    {
+        text.text = "Ping: " + PhotonNetwork.GetPing().ToString();
+    }
+}
