af8a5259c73301ed0d77419ea202819e7079acc2
Sebastian Rueda <jrueda275@unab.edu.co>
2019-12-21 01:42:17 -0500

    new moving wall skill

---
+++ Souls Link/Assets/Scripts/Character/Chimera/TankDruid/MovingWall.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingWall : Skill
+{
+    
+    public GameObject _wallPrefab;
+    public float _speed = 6f;
+    private float _coolDownTracker = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _coolDownTracker = _coolDown;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (_coolDownTracker <= _coolDown && _coolDownTracker > 0)
+        {
+            _coolDownTracker -= Time.deltaTime;
+        }
+
+    }
+
+    public void spawnGroundSmash()
+    {
+        if (_coolDownTracker <= 0)
+        {
+            Vector2 direction = GetComponent<PlayerAiming>().AimDirection;
+            _coolDownTracker = _coolDown;
+            GameObject temp = Instantiate(_wallPrefab, transform.position, Quaternion.identity);
+            temp.GetComponentInChildren<MovingWallController>().setWall(direction, _speed);
+
+        }
+    }
+
+    private void stopMoving()
+    {
+        GetComponent<PlayerMovement>().enabled = false;
+        GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Static;
+        GetComponentInChildren<Animator>().SetBool("isCasting", true);
+    }
+
+    public void backToNormal()
+    {
+        GetComponent<PlayerMovement>().enabled = true;
+        GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Dynamic;
+        GetComponentInChildren<Animator>().SetBool("isCasting", false);
+    }
+}
