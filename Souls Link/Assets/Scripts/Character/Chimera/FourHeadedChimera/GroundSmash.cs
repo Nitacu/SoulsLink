7719ad7beefacd32c9e6562be869e81906c1718d
Sebastian Rueda <jrueda275@unab.edu.co>
2019-12-21 00:43:31 -0500

    Added ground smash

---
+++ Souls Link/Assets/Scripts/Character/Chimera/FourHeadedChimera/GroundSmash.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroundSmash : Skill
+{
+
+    public float _damage = 200;
+    public GameObject _rockSmashPrefab;
+    private float _coolDownTracker = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _coolDownTracker = _coolDown;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (_coolDownTracker <= _coolDown && _coolDownTracker > 0)
+        {
+            _coolDownTracker -= Time.deltaTime;
+        }
+
+    }
+
+    public void spawnGroundSmash()
+    {
+        if (_coolDownTracker <= 0)
+        {
+            Vector2 direction = GetComponent<PlayerAiming>().AimDirection;
+            _coolDownTracker = _coolDown;
+            GameObject temp = Instantiate(_rockSmashPrefab, transform.position, Quaternion.identity);
+            stopMoving();
+            temp.GetComponentInChildren<GroundSmashController>().setGroundSmash(_damage, gameObject);
+            
+        }
+    }
+
+    private void stopMoving()
+    {
+        GetComponent<PlayerMovement>().enabled = false;
+        GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Static;
+        GetComponentInChildren<Animator>().SetBool("isCasting", true);
+    }
+
+    public void backToNormal()
+    {
+        GetComponent<PlayerMovement>().enabled = true;
+        GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Dynamic;
+        GetComponentInChildren<Animator>().SetBool("isCasting", false);
+    }
+
+}
---
+++ Souls Link/Assets/Scripts/Character/Chimera/FourHeadedChimera/GroundSmashController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroundSmashController : MonoBehaviour
+{
+    public float _damage = 100;
+    private GameObject playerReference;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Enemy"))
+        {
+            collision.gameObject.GetComponent<SimpleEnemyController>().recieveDamage(_damage);
+        }
+    }
+
+    public void setGroundSmash(float damage, GameObject _player)
+    {
+        playerReference = _player;
+        _damage = damage;
+    }
+
+    private void destoyMyself()
+    {
+        playerReference.GetComponent<GroundSmash>().backToNormal();
+        Destroy(gameObject);
+    }
+}
