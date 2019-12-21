af89062955187112c53db4ac66db6c574e3654da
Sebastian Rueda <jrueda275@unab.edu.co>
2019-12-20 22:43:06 -0500

    NewChimeraSkillAdded

---
+++ Souls Link/Assets/Scripts/Character/Chimera/FourHeadedChimera/BloodSuckerController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BloodSuckerController : MonoBehaviour
+{
+    
+    private GameObject playerReference;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Enemy"))
+        {
+            playerReference.GetComponent<BloodSucker>().dealDamage(collision.gameObject);
+        }
+    }
+
+    public void setBloodSucker(GameObject _player)
+    {
+        playerReference = _player;       
+    }
+
+}
